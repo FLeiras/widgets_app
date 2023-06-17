a5904e2c007f3d6ba98113ded711b316a485cd3c
FLeiras <fleiras18@gmail.com>
2023-06-17 13:58:23 -0300

    rutas basicas configuradas

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -41,17 +41,18 @@ class _CustomListTitle extends StatelessWidget {
     final colors = Theme.of(context).colorScheme;
 
     return ListTile(
-      leading: Icon(
-        menuItem.icon,
-        color: colors.primary,
-      ),
-      trailing: Icon(
-        Icons.arrow_forward_ios_rounded,
-        color: colors.primary,
-      ),
-      title: Text(menuItem.title),
-      subtitle: Text(menuItem.subTitle),
-      onTap: () {},
-    );
+        leading: Icon(
+          menuItem.icon,
+          color: colors.primary,
+        ),
+        trailing: Icon(
+          Icons.arrow_forward_ios_rounded,
+          color: colors.primary,
+        ),
+        title: Text(menuItem.title),
+        subtitle: Text(menuItem.subTitle),
+        onTap: () {
+          Navigator.pushNamed(context, menuItem.link);
+        });
   }
 }
